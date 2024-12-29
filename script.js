22a173121d0422697c7e95652e7de2bca3a2fe3f
Pr75502 <pp1614582@gmail.com>
2024-12-29 22:30:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let charMap=new Map()
+	for(let t of str){
+		if(charMap.has (t)){
+			charMap.set(t,charMap.get(t)+1)
+		}else{
+			charMap.set(char,1)
+		}
+	}
+for(let t of str){
+	if(charMap.get(t)==1){
+		return t
+	}
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+return null	
+}
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
